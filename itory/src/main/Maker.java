8c3186a510de68079be7e76f312a9cd9494e5967
= <paphra.me@gmail.com>
2018-11-19 11:30:49 +0300

    sorry, take this

--- itory/src/main/Maker.java
+++ itory/src/main/Maker.java
@@ -1,5 +1,9 @@
 package main;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 public class Maker {
@@ -11,5 +15,25 @@ public class Maker {
     // border pane
     public BorderPane bp() {
         return new BorderPane();
+    }
+
+    // menu bar
+    public MenuBar mb() {
+        return new MenuBar();
+    }
+
+    // menu
+    public Menu menu(String name) {
+        return new Menu(name);
+    }
+
+    // menu item
+    public MenuItem mi(String name) {
+        return new MenuItem(name);
+    }
+
+    // button
+    public Button btn(String text) {
+        return new Button(text);
     }
 }
